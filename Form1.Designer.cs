3953e7d45d87212d371209fe9eaac541232bf7d2
DESKTOP-QQRIUTH\Alexandr <error2221@gmail.com>
2020-02-02 00:02:00 +0300

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace NeedSomeSleep
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Текст вопроса";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(19, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(68, 44);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(93, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 44);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "ОК";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(264, 135);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Location = new System.Drawing.Point(0, 100);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Need some sleep";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NeedSomeSleep
+{
+    public partial class Form1 : Form
+    {
+        static int minute = 1000 * 60;
+        int intervalAfterHide = minute * 15;
+        int intervalAfterShow = minute;
+        string[] questions;
+
+        public Form1()
+        {
+            InitializeComponent();
+            addMessageToLog("Запуск программы");
+            timer1.Interval = intervalAfterShow; 
+            timer1.Enabled = true;
+            timer1.Tick += timer1_Tick;
+            TopMost = true;
+            ShowIcon = false;
+            string path = Directory.GetCurrentDirectory() + "\\questions.txt";
+            string pathSettings = Directory.GetCurrentDirectory() + "\\settings.ini";
+            try
+            {
+                questions = File.ReadAllLines(path, Encoding.UTF8);
+                updateQuestion();
+            }
+            catch
+            {
+                addMessageToLog($"Не найден файл с вопросами {path}, создайте его (по одному вопросу на каждой строке)");
+            }
+
+            try
+            {
+                var settings = File.ReadAllLines(pathSettings, Encoding.UTF8);
+                intervalAfterHide = Int32.Parse(settings[1]) * minute;
+                intervalAfterShow = Int32.Parse(settings[3]) + minute;
+            }
+            catch
+            {
+                addMessageToLog($"Не найден файл с настройками {pathSettings}, применены настройки по умолчанию");
+            }
+
+        }
+
+        void BacklogProcedure()
+        {
+
+        }
+
+        void timer1_Tick(object sender, EventArgs e)
+        {
+            var now = DateTime.Now;
+
+            bool doWork = (0 <= now.Hour && now.Hour <= 7);
+            if (!doWork)
+            {
+                addMessageToLog("Программа спит, т.к. сейчас не время контроля");
+                return;
+            }
+                
+
+
+            //если форма показывается
+            if (this.Visible)
+            {
+                //15 секунд пырился в нее и ничего не ответил
+                if (textBox1.Text.Equals(""))
+                    addMessageToLog("за отведенное время ответ не был дан!");
+                else
+                {
+                    addMessageToLog("ОК нажат не был, но ответ был дан");
+                    getAnswer();
+                }
+                    
+                //то скрываем ее на определенное время
+                this.Hide();
+                timer1.Interval = intervalAfterHide;
+            }
+            
+            //если форма скрыта
+            else
+            {
+                updateQuestion();
+                //то показываем ее ровно на 15 секунд для ответа
+                this.Show();
+                timer1.Interval= intervalAfterShow;
+            }
+                
+        }
+
+        private void updateQuestion()
+        {
+            if (questions is null || questions.Length == 0)
+                return;
+            Random rnd = new Random();
+
+            //Получить очередное (в данном случае - первое) случайное число
+            int value = rnd.Next(questions.Length);
+
+            label1.Text = questions[value] + " ?";
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            getAnswer();
+        }
+
+        private void getAnswer()
+        {
+            string answer = $"вопрос: { label1.Text}; ответ: { textBox1.Text}";
+            addMessageToLog(answer);
+            textBox1.Text = "";
+            this.Hide();
+            timer1.Interval = intervalAfterHide;
+        }
+
+        private void addMessageToLog(string message)
+        {
+            Directory.CreateDirectory(Directory.GetCurrentDirectory() + "\\answers");
+            string currentLog = Directory.GetCurrentDirectory() + "\\answers\\" + $"{DateTime.Now.ToString("yyyy-MM-dd")}.txt";
+            File.AppendAllText(currentLog, $"[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}] {message}{Environment.NewLine}");
+            
+
+        }
+
+    }
+}
